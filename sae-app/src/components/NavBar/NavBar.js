7667acdf1d9d80a3d87e96fb27f81e53b4f12076
Marcelo Rodriguez <84423976+bakiUru@users.noreply.github.com>
2021-09-15 22:05:01 -0300

    NavBar
    NavBar agregado

---
+++ sae-app/src/components/NavBar/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './NavBar.css';
+
+export default function NavBar() {
+    return(
+        <div>
+            <nav>
+            <ul>
+                <li><a href="#">Riego</a></li>
+                <li><a href="#">Cultivo</a></li>
+                <li><a href="#">Domotica</a></li>
+                <li><a href="#">Contacto</a></li>
+                </ul>
+            </nav>
+        </div>
+    );
+}
